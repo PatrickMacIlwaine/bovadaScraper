4237923eb264a8298a562d3e87cb1f45ac7afdd0
Patrick MacIlwaine <patrick@macilwaine.com>
2022-05-22 12:08:21 -0500

    wrote a readme file

---
+++ readme.py
@@ -0,0 +1,12 @@
+"""
+This project is about beating the gambling websites.
+
+Note that for the scrapers to work you must vpn to a state where it is legal. For example, in Italy I was unable
+to scrape because Bovada is banned in Italy. Once I VPNed to California everything worked fine.
+
+Currently, this project only works with Starcraft 2, as the database I am using to compare is Alligulac.
+
+yeet.
+
+
+"""
