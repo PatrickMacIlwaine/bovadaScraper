e5e35a9d17ab1297cada1fc95298a7b1ac955156
Patrick MacIlwaine <patrick@macilwaine.com>
2022-08-24 11:18:23 -0500

    Update readme.py

--- readme.py
+++ readme.py
@@ -7,4 +7,6 @@ The reason I made this program is to better understand how the gambiling website
 
 The program will output the estimated return for each bet based on Aligulac betting odds. 
 
+To run the program run main.py.
+
 """
